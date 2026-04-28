46126425b0b0ff9f59a6ef747f5c3e038d920cdb
aaron-zeile <azeile@ucsc.edu>
2026-04-28 12:19:17 -0700

    added name to page

--- App/helloworld/app/page.tsx
+++ App/helloworld/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center text-4xl">
       <div>Hello World</div>
       <div>Pouria Rezaei</div>
+      <div>Aaron Zeile</div>
     </div>
   );
 }
