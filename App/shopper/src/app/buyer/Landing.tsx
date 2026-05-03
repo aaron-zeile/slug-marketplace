e949e55c80951aac3a4ed3f116c95da34b4f63ca
SkenorDev <nathanskinner@comcast.net>
2026-05-03 12:09:42 -0700

    more fixes

--- App/shopper/src/app/buyer/Landing.tsx
+++ App/shopper/src/app/buyer/Landing.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { GoogleLogin } from "@react-oauth/google";
-
+//If it says auth error before logging in go to https://console.cloud.google.com/auth/clients?project=slugmarketplace to 
+//fix url settings
 export function Landing() {
   return (
     <GoogleLogin
