e22f64a3aeb9222499be32a14f574fd8945dfb16
aaron-zeile <azeile@ucsc.edu>
2026-05-03 13:26:21 -0700

    added server and shared files for seller

---
+++ App/seller/server/index.ts
@@ -0,0 +1,24 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import express from 'express'
+
+import app from './app.ts'
+
+// TODO fix port number??
+const port = Number(process.env.PORT ?? 3010);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const clientDistDir = path.resolve(__dirname, '../client');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(clientDistDir));
+  app.get(/.*/, (_request, response) => {
+    response.sendFile(path.join(clientDistDir, 'index.html'));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${String(port)}`);
+});
