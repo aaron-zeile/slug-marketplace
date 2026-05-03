e546b23962dc1494cbaf34551e102bce7a6b649c
aaron-zeile <azeile@ucsc.edu>
2026-05-03 13:27:14 -0700

    uncommented middleware

--- App/seller/server/middleware.ts
+++ App/seller/server/middleware.ts
@@ -1,16 +1,16 @@
-// import type { Request, Response, NextFunction } from "express";
-// import type { z } from "zod";
+import type { Request, Response, NextFunction } from "express";
+import type { z } from "zod";
 
-// export const validate = (schema: z.ZodObject) => 
-//   (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//       schema.parse({ 
-//         body: req.body as string, 
-//         query: req.query, 
-//         params: req.params 
-//       })
-//       next()
-//     } catch (error) {
-//       return res.status(400).json(error)
-//     }
-// }
+export const validate = (schema: z.ZodObject) => 
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({ 
+        body: req.body as string, 
+        query: req.query, 
+        params: req.params 
+      })
+      next()
+    } catch (error) {
+      return res.status(400).json(error)
+    }
+}
