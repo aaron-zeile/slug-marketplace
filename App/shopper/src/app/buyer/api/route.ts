171672a5118a545b42f108d21f15ef09ce54ff93
unknown <rlarthur@ucsc.edu>
2026-05-13 11:46:14 -0700

    build fix?

--- App/shopper/src/app/buyer/api/route.ts
+++ App/shopper/src/app/buyer/api/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request: NextRequest) {
+  const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
   try {
     const { amount } = await request.json();
 
