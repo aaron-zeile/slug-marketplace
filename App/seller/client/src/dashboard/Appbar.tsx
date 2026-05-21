c168c00cec0f8e6ccf6ed46d61182164f1b585ff
aaron-zeile <azeile@ucsc.edu>
2026-05-21 16:58:23 -0700

    started adding seller menu (for switching back to shopper)

--- App/seller/client/src/dashboard/Appbar.tsx
+++ App/seller/client/src/dashboard/Appbar.tsx
@@ -4,24 +4,50 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import NavTabs from './Tabs'
 import LocaleSwitcher from '../components/LocaleSwitcher'
 
 export default function TopBar() {
   const t = useTranslations('App')
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position='sticky'>
       <Toolbar>
         <IconButton
+            onClick={open ? handleClose : handleClick}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-expanded={open}
             size="large"
             edge="start"
             color="inherit"
             aria-label={t('openMenu')}
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <Avatar />
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+            >
+              <MenuItem>
+                <ShoppingCartIcon/> Slug Marketplace
+              </MenuItem>
+            </Menu>
         </IconButton>
         <Typography sx={{ flexGrow: 1 }}>
           {t('title')}
